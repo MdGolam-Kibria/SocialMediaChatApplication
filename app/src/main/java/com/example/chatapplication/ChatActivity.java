ef399672fae5a099083d5b17242923852aae2f8d
Kibria92 <kibria92@gmail.com>
2020-06-10 19:01:52 +0600

    send message add

--- app/src/main/java/com/example/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatActivity.java
@@ -1,23 +1,47 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
 public class ChatActivity extends AppCompatActivity {
-Toolbar toolbar;
-RecyclerView recyclerView;
-ImageView profileIv;
-TextView nameTv,userStatusTv;
-EditText messageEt;
-ImageButton sendBtn;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    ImageView profileIv;
+    TextView nameTv, userStatusTv;
+    EditText messageEt;
+    ImageButton sendBtn;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference usersDbRef;
+    String hisUid;
+    String myUid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +50,124 @@ ImageButton sendBtn;
         setSupportActionBar(toolbar);
         toolbar.setTitle("");
         recyclerView = findViewById(R.id.chat_recyclerView);
-        profileIv  = findViewById(R.id.profileIv);
+        profileIv = findViewById(R.id.profileIv);
         nameTv = findViewById(R.id.nameTv);
         userStatusTv = findViewById(R.id.userStatusTv);
         messageEt = findViewById(R.id.messageEt);
         sendBtn = findViewById(R.id.sendBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usersDbRef = firebaseDatabase.getReference("Users");
+        //search user to get that user's info
+        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
+        //get user picture and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check untill required info is rechived
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //get data
+                    String name = "" + ds.child("name").getValue();
+                    String image = "" + ds.child("image").getValue();
 
+                    //set data
+                    nameTv.setText(name);
+                    try {
+                        //image received set it to imageview in toolbar
+                        Picasso.get().load(image).placeholder(R.drawable.ic_default).into(profileIv);
+
+                    } catch (Exception e) {
+                        Picasso.get().load(R.drawable.ic_face_img).into(profileIv);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        //click send button to send message
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEt.getText().toString().trim();
+                if (TextUtils.isEmpty(message)) {
+                    messageEt.setError("message is empty");
+                    messageEt.requestFocus();
+                    return;
+                } else {
+                    sendMessage(message);
+                }
+            }
+        });
+
+        /*
+        On clicking user from users list we have passed that's user UID using intent
+        *so get that UID here to get the profile picture, name and start chat with that
+        *user*/
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("hisUid");
+    }
+
+    private void sendMessage(String message) {
+
+        /*
+        "Chats" node will be created that will contains all chats
+        * whenever user send message  it will create new child in firebase database in "Chats" node and that child will contain
+        * that follwing  key values
+        *sender : UID of sender
+        * rechiver : UID of receiver
+        * message : The actual message
+         */
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message", message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+        //reset editText after sending message
+        messageEt.setText("");
+    }
+
+    private void checkUserStatus() {//check user sign in or not and accessibility
+        FirebaseUser user = firebaseAuth.getCurrentUser();//get current user
+        if (user != null) {
+            //user signed in stay here
+//            mProfileTv.setText(user.getEmail());
+            myUid = user.getUid();//currently signed in user's UID
+        } else {
+            //user not signed in
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        //hide searchview as we don't need it here
+        menu.findItem(R.id.action_search).setVisible(false);//remove search icon from here......
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.actionLogout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
