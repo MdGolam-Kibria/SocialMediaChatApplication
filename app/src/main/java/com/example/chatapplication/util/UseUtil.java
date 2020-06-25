c861144362626bbd170b32aeda39d1d0a6a0d099
Kibria92 <kibria92@gmail.com>
2020-06-26 01:49:06 +0600

    check network clint in util

--- app/src/main/java/com/example/chatapplication/util/UseUtil.java
+++ app/src/main/java/com/example/chatapplication/util/UseUtil.java
@@ -1,6 +1,10 @@
 package com.example.chatapplication.util;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.example.chatapplication.R;
 
 public class UseUtil {
@@ -8,4 +12,11 @@ public class UseUtil {
     public static int changeFragmentAnimation2nd = R.animator.exit_to_left;
     public static int changeFragmentAnimation3rd = R.animator.enter_from_right;
     public static int changeFragmentAnimation4th = R.animator.exit_to_left;
+
+    private static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
